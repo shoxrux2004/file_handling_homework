d42d4c9ae4343525750bd6add1b139a14a879af8
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-08-05 20:50:54 +0500

    filedagi satr uzunligi

--- file02.py
+++ file02.py
@@ -6,5 +6,7 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
-# Read data from file
+    f=open("data/data02.txt")
+    data=len(f.read())
+    return data
+print(main("data02.txt"))
