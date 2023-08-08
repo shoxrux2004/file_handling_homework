731cb1d4268b9a1be0232addcbbee30a016cc755
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-08-08 11:40:01 +0500

    max find

--- file08.py
+++ file08.py
@@ -6,5 +6,11 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
-# Read data from file
+    l=[]
+    for i in range(len(data)):
+        if data[i].isdigit()==True:
+            l.append(data[i])
+    a=max(l)
+    return a
+f=open("data/data08.txt").read()
+print(main(f))
